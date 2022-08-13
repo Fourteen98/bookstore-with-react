12fc119ee4dfe5f458e66b8e0e907f42d87aa1b2
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-08-13 09:23:26 +0000

    added bookstor components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import {
+  BrowserRouter as Router, Routes,
+  Route,
+} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Books from './components/Books';
+import Category from './components/Category';
+import Form from './components/Form';
+
 function App() {
   return (
     <div>
-
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/category" element={<Category />} />
+          <Route path="/form" element={<Form />} />
+        </Routes>
+      </Router>
     </div>
   );
 }

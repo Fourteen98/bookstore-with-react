0682d840d7daf522626186546ccdddd5a912189a
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-08-18 15:41:22 +0000

    dispatch bookinfo state to reducer dispatch

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,24 @@
 import { useSelector, useDispatch } from 'react-redux';
+import { useState } from 'react';
 import { addBook } from '../redux/books/books';
 
 const Form = () => {
+  const [bookInfo, setBookInfo] = useState({ title: '', author: '' });
   const books = useSelector((state) => state.books);
   const dispatch = useDispatch();
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    dispatch(addBook(books.length + 1, bookInfo.title, bookInfo.author));
+  };
+
+  const handleChange = (event) => {
+    setBookInfo({
+      ...bookInfo,
+      [event.target.name]: event.target.value,
+    });
+  };
+
   return (
     <form className="form">
       <input
@@ -11,32 +26,20 @@ const Form = () => {
         name="title"
         placeholder="Author"
         className="title"
+        onChange={handleChange}
       />
       <input
         type="text"
         name="author"
         placeholder="Book"
         className="author"
+        onChange={handleChange}
       />
       <input
         type="submit"
         value="Submit"
         className="submit"
-        onClick={(event) => {
-          event.preventDefault();
-          const title = event.target.previousElementSibling.value;
-          const author = event.target.previousElementSibling.previousElementSibling.value;
-          if (title && author) {
-            dispatch(addBook(books.length + 1, title, author));
-            // eslint-disable-next-line no-param-reassign
-            event.target.previousElementSibling.previousElementSibling.value = '';
-            // eslint-disable-next-line no-param-reassign
-            event.target.previousElementSibling.value = '';
-          } else {
-            // eslint-disable-next-line no-alert
-            alert('Please fill in all fields');
-          }
-        }}
+        onClick={handleClick}
       />
 
     </form>
@@ -44,3 +47,18 @@ const Form = () => {
 };
 
 export default Form;
+
+// (event) => {
+//   event.preventDefault();
+//   const title = event.target.previousElementSibling.value;
+//   const author = event.target.previousElementSibling.previousElementSibling.value;
+//   if (title && author) {
+//     dispatch(addBook(books.length + 1, title, author));
+//     // eslint-disable-next-line no-param-reassign
+//     event.target.previousElementSibling.previousElementSibling.value = '';
+//     // eslint-disable-next-line no-param-reassign
+//     event.target.previousElementSibling.value = '';
+//   } else {
+//     alert('Please fill in all fields');
+//   }
+// }}

56d325a5696254cc824aecc6fda6c77c8ed48d9b
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-08-18 09:27:41 +0000

    implement add functionality

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,7 +1,14 @@
-const Category = () => (
-  <div>
-    <button type="button">Check status</button>
-  </div>
-);
+import { useSelector, useDispatch } from 'react-redux/es/exports';
+import { CheckStatus } from '../redux/categories/categories';
 
+const Category = () => {
+  const response = useSelector((state) => state.categoriesReducer);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <button type="button" onClick={() => dispatch(CheckStatus())}>Check status</button>
+      <h1>{response}</h1>
+    </div>
+  );
+};
 export default Category;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,45 @@
-const Form = () => (
-  <form className="form">
-    <input type="text" name="author" placeholder="Author" className="author" />
-    <input type="text" name="book" placeholder="Book" className="book" />
-    <input type="submit" value="Submit" className="submit" />
-  </form>
-);
+import { useSelector, useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
+
+const Form = () => {
+  const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  return (
+    <form className="form">
+      <input
+        type="text"
+        name="title"
+        placeholder="Author"
+        className="title"
+      />
+      <input
+        type="text"
+        name="author"
+        placeholder="Book"
+        className="author"
+      />
+      <input
+        type="submit"
+        value="Submit"
+        className="submit"
+        onClick={(event) => {
+          event.preventDefault();
+          const title = event.target.previousElementSibling.value;
+          const author = event.target.previousElementSibling.previousElementSibling.value;
+          if (title && author) {
+            dispatch(addBook(books.length + 1, title, author));
+            // eslint-disable-next-line no-param-reassign
+            event.target.previousElementSibling.previousElementSibling.value = '';
+            // eslint-disable-next-line no-param-reassign
+            event.target.previousElementSibling.value = '';
+          } else {
+            alert('Please fill in all fields');
+          }
+        }}
+      />
+
+    </form>
+  );
+};
 
 export default Form;
